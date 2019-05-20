0c2607b2ed261a4e423474c5e27149da4dbc5804
antonio <afj014@inlumine.ual.es>
2019-05-20 21:28:35 +0200

    Añade * a la cabecera

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -69,7 +69,7 @@ func signInController(w http.ResponseWriter, req *http.Request) {
 }
 
 func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST,GET,OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
